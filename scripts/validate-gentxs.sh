5229e9ea14b7ef9e472155338a159d5f724ed841
Prajjwol Gautam <not@acyb.org>
2021-11-17 09:34:20 -0800

    ci: fix gentx action task with missing env variables

--- scripts/validate-gentxs.sh
+++ scripts/validate-gentxs.sh
@@ -19,39 +19,55 @@ echo
 
 if [[ -z "${GH_URL}" ]]; then
   echo "GH_URL in not set, required. Ex: https://github.com/tharsis/evmos"
-  exit 0
+  exit 1
 fi
 if [[ -z "${DAEMON}" ]]; then
   echo "DAEMON is not set, required. Ex: evmosd, gaiad etc"
-  exit 0
+  exit 1
 fi
 if [[ -z "${DENOM}" ]]; then
   echo "DENOM in not set, required. Ex: stake, aphoton etc"
-  exit 0
+  exit 1
 fi
 if [[ -z "${GO_VERSION}" ]]; then
   echo "GO_VERSION in not set, required. Ex: 1.15.2, 1.16.6 etc."
-  exit 0
+  exit 1
 fi
 if [[ -z "${CHAIN_ID}" ]]; then
   echo "CHAIN_ID in not set, required."
-  exit 0
+  exit 1
 fi
 if [[ -z "${PRELAUNCH_GENESIS_URL}" ]]; then
   echo "PRELAUNCH_GENESIS_URL (genesis file url) in not set, required."
-  exit 0
+  exit 1
 fi
 if [[ -z "${GENTXS_DIR}" ]]; then
   echo "GENTXS_DIR in not set, required."
-  exit 0
+  exit 1
 fi
 
 command_exists () {
     type "$1" &> /dev/null ;
 }
 
+setup_go_env () {
+  echo "" >> ~/.profile
+  echo 'export GOPATH=$HOME/go' >> ~/.profile
+  echo 'export GOROOT=/usr/local/go' >> ~/.profile
+  echo 'export GOBIN=$GOPATH/bin' >> ~/.profile
+  echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.profile
+
+  . ~/.profile
+}
+
 if command_exists go ; then
-    echo "Golang is already installed"
+  echo "Golang is already installed"
+
+  if [[ -z "${GOPATH}" ]]; then
+    echo "GOPATH is not set. Setting it up."
+    setup_go_env
+  fi
+
 else
   read -s -p "Installing go using apt. Do you want to proceed (y/n)?: " useApt
 
@@ -68,13 +84,7 @@ else
   tar -xvf go$GO_VERSION.linux-amd64.tar.gz
   sudo mv go /usr/local
 
-  echo "" >> ~/.profile
-  echo 'export GOPATH=$HOME/go' >> ~/.profile
-  echo 'export GOROOT=/usr/local/go' >> ~/.profile
-  echo 'export GOBIN=$GOPATH/bin' >> ~/.profile
-  echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.profile
-
-  . ~/.profile
+  setup_go_env
 
   go version
 fi
